0821699f14953eeb4842b0a56cd50d01e06639d4
Ilnaz <153454492+irunazushan@users.noreply.github.com>
2024-04-26 22:06:22 +0300

    Update build.sh

--- FastTrackTelecom/task_4/rrc/build.sh
+++ FastTrackTelecom/task_4/rrc/build.sh
@@ -1,9 +1,9 @@
 rm -r src/*
 rm -r build/*
-# mkdir src
-# mkdir build
-# asn1c rrc.asn1 -D src -pdu=auto -no-gen-OER -fcompound-names -no-gen-example -fno-include-deps
-# gcc -g -Isrc -I. -o build/rrc_coder main.c rrc_coder.c src/*.c -lsctp -DPDU=auto -DASN_DISABLE_OER_SUPPORT
-# ./build/server
-# gcc -g -Isrc -I. -o build/server server/server.c src/*.c -lsctp -DPDU=auto -DASN_DISABLE_OER_SUPPORT
-# ./build/rrc_coder
+mkdir src
+mkdir build
+asn1c rrc.asn1 -D src -pdu=auto -no-gen-OER -fcompound-names -no-gen-example -fno-include-deps
+gcc -g -Isrc -I. -o build/rrc_coder main.c rrc_coder.c src/*.c -lsctp -DPDU=auto -DASN_DISABLE_OER_SUPPORT
+./build/server
+gcc -g -Isrc -I. -o build/server server/server.c src/*.c -lsctp -DPDU=auto -DASN_DISABLE_OER_SUPPORT
+./build/rrc_coder
